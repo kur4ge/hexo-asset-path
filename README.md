aa4cf30a219741135e5493486d18c7c5ccc447ea
r12f <r12f.code@gmail.com>
2017-02-05 09:57:36 -0800

    Initial checkin.

---
+++ README.md
@@ -0,0 +1,67 @@
+# hexo-tag-asset-path
+
+[![npm version](https://badge.fury.io/js/hexo-tag-asset-path.svg)](http://badge.fury.io/js/hexo-tag-asset-path)
+
+In hexo, the images inserted by markdown syntax are often not points to the right place, due to relative path. We can use absolute path to work around this issue. But inserting the post permalink or asset folder every time is very annoying. Also, sometimes we don't like to store that much data on github, we like to move all the images to another CDN. It could be really painful, if you need to update every posts you wrote!
+
+This plugin is written to deal with all these issues. It is trying to make inserting assets to post and integrating with CDN a lot more easier.
+
+## Installation
+
+``` bash
+$ npm install hexo-tag-asset-path --save
+```
+
+## Usage
+
+### Setting up the configuration in _config.ymal
+```
+asset_path:
+  enable: true
+
+  # Asset folder, used when post asset folder is not enabled.
+  # For example, the following asset folder configuration will make hexo to use "/r12f-assets/post-assets/2017-01-16-Hello-World" as the base URL in one of my post.
+  asset_folder: /r12f-assets/post_assets/<% print(post_created.format('YYYY-MM-DD')) %>-<%= post_slug %>
+
+  # CDN settings, and CDN folder will always be considered to have its own domain and start from the root.
+  enable_cdn: true
+  cdn_folder: r12f-cdn.azureedge.net/r12f-assets/post_assets/<% print(post_created.format('YYYY-MM-DD')) %>-<%= post_slug %>
+  cdn_use_https: false
+
+  # Selectors and attribute names for getting the elements to update.
+  selectors:
+    img[src]: 'src'
+```
+
+Variables you can use in the asset folder and cdn folder:
+* post_title
+* post_slug
+* post_created
+* post_created_date
+* post_created_time
+
+The variables are supported by underscore template, so you can even execute arbitrary JavaScript code with <% %>. For example, <% print(post_created.format('YYYY-MM-DD') %> will give you 2017-01-01, if today is that day.
+
+### Inserting assets
+Just use the markdown syntax to insert your images, with the relative path (to the post asset folder if enabled, otherwise to your asset folder specified in hexo-tag-asset-path configuration).
+```
+![your image](path/to/image.png)
+```
+
+### Asset path in different modes
+As we know, we can enable post asset folder in hexo, and hexo can create a local server to preview your blog, and you might like to enable CDN when you generate the real site. The asset path for showing your assets properly in these different modes are actually different. And the following table shows how we generate the asset path:
+
+|                            | Local server             | Real site                                              |
+| -------------------------- | ------------------------ | ------------------------------------------------------ |
+| Post asset folder enabled  | /PostPermalink/AssetPath | (CDN enabled ? //CDNFolder : )/PostPermalink/AssetPath |
+| Post asset folder disabled | /AssetFolder/AssetPath   | (CDN enabled ? //CDNFolder : /AssetFolder)/AssetPath   |
+
+### Debugging
+The posts and links we updated is outputted to the hexo log. Just simply enable debug mode and logging in hexo, then you can see it.
+```
+hexo s --debug --log
+```
+
+## License
+
+BSD v3
---
+++ index.js
@@ -0,0 +1,131 @@
+'use strict';
+
+var path = require('path');
+var soup = require('soup');
+var _ = require('underscore');
+var log = hexo.log || log.log;
+
+var postKeys = {
+    'post_title' : (config) => config.title,
+    'post_slug' : (config) => config.slug,
+    'post_created' : (config) => config.date,
+    'post_created_date' : (config) => config.date.format(hexo.config.date_format),
+    'post_created_time' : (config) => config.date.format(hexo.config.time_format),
+};
+
+hexo.extend.filter.register('after_post_render', function(postInfo) {
+    var assetPathConfig = hexo.config.asset_path;
+    if (!assetPathConfig || !assetPathConfig.enable) {
+        return;
+    }
+
+    validateConfigurationsAndFailFast(assetPathConfig);
+
+    var options = {
+        enable: assetPathConfig.enable,
+        createAssetFolder: assetPathConfig.asset_folder ? _.template(assetPathConfig.asset_folder) : null,
+        enableCDN: assetPathConfig.enable_cdn,
+        createCDNFolder: assetPathConfig.enable_cdn ? _.template(assetPathConfig.cdn_folder) : null,
+        cdnUseHttps: assetPathConfig.cdn_use_https,
+        selectors: assetPathConfig.selectors ? assetPathConfig.selectors : null,
+        isPostAssetFolderEnabled: hexo.config.post_asset_folder,
+        isRunningInLocalServerMode: (process.argv.indexOf('server') > -1 || process.argv.indexOf('s') > -1)
+    };
+
+    // console.log(options);
+    // console.log(postInfo);
+
+    var contentKeys = ['content', 'more', 'excerpt'];
+    contentKeys.forEach((contentKey) => {
+        postInfo[contentKey] = getPostHtmlWithFixedAssetPaths(options, postInfo, postInfo[contentKey]);
+    });
+
+    log.log("Update Asset Links: " + postInfo.path);
+});
+
+function validateConfigurationsAndFailFast(assetPathConfig) {
+    if (!hexo.config.post_asset_folder && !assetPathConfig.asset_folder) {
+        throw new Error("Post asset folder is not enabled, please specify the asset_folder in the configuration.");
+    }
+
+    if (assetPathConfig.enable_cdn && !assetPathConfig.cdn_folder) {
+        throw new Error("CDN is enabled, please specify the cdn_folder in the configurations.");
+    }
+}
+
+function getPostHtmlWithFixedAssetPaths(options, postInfo, postContent) {
+    var postSoup = new soup(postContent);
+
+    Object.keys(options.selectors).forEach((selector) => {
+        var attributeName = options.selectors[selector];
+        if (attributeName) {
+            postSoup.setAttribute(selector, attributeName, (assetPath) => {
+                var fixedAssetPath = fixAssetPath(options, postInfo, assetPath);
+                log.log('Asset link fixed: ' + fixedAssetPath);
+                return fixedAssetPath;
+            });
+        }
+    });
+
+    return postSoup.toString();
+}
+
+function fixAssetPath(options, postInfo, assetPath) {
+    if (!assetPath || assetPath.length == 0) {
+        return assetPath;
+    }
+
+    // If the link is a data URI, we don't need to do anything.
+    if (isDataURI(assetPath)) {
+        return assetPath;
+    }
+
+    // If the link is an absolute URL, we don't need to do anything, because the writer should be confident about where to load the asset.
+    if (isAbsoluteURL(assetPath)) {
+        return assetPath;
+    }
+
+    // Update the asset link based on if we enabled post asset folder and our running mode.
+    // The things we need to do in each mode are listed below:
+    //                              LocalServer             RealSite
+    // PostAssetFolderEnabled       /Permalink/AssetPath    (//CDNFolder||)/Permalink/AssetPath
+    // PostAssetFolderDisabled      /AssetFolder/AssetPath  (//CDNFolder||/AssetFolder)/AssetPath
+    //
+    // Note: The permalink of the post doesn't contain the index.html, so we can directly use it as a folder.
+    var fixedAssetPath = assetPath;
+
+    var usePermalink = options.isPostAssetFolderEnabled;
+    if (usePermalink) {
+        fixedAssetPath = path.join(postInfo.permalink, assetPath);
+    }
+
+    var useAssetFolder = ((options.isRunningInLocalServerMode && !options.isPostAssetFolderEnabled) || !options.enableCDN);
+    var useCDNFolder = (!options.isRunningInLocalServerMode && options.enableCDN);
+    if (useAssetFolder) {
+        var assetFolderArguments = createFolderTemplateArguments(postInfo);
+        var assetFolder = options.createAssetFolder(assetFolderArguments);
+        fixedAssetPath = path.join(assetFolder, fixedAssetPath);
+    } else if (useCDNFolder) {
+        var cdnFolderArguments = createFolderTemplateArguments(postInfo);
+        var cdnFolder = options.createCDNFolder(cdnFolderArguments);
+        fixedAssetPath = path.join(cdnFolder, fixedAssetPath);
+        fixedAssetPath = [ options.cdnUseHttps ? "https://" : "http://", fixedAssetPath ].join('');
+    }
+
+    // Fix the path separators from windows.
+    return fixedAssetPath.replace(/\\/g, '/');
+}
+
+function isDataURI(link) {
+    return link.indexOf('data:') === 0;
+}
+
+function isAbsoluteURL(link) {
+    return link.indexOf('/') === 0 || link.indexOf('://') >= 0;
+}
+
+function createFolderTemplateArguments(postInfo) {
+    var templateArgs = {};
+    Object.keys(postKeys).forEach((key) => { templateArgs[key] = postKeys[key](postInfo); });
+    return templateArgs;
+}
